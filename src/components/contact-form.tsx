0a4c5170fa3bab6666e63c08e812e4df87dbab17
Smart Ideas <smartideasbd24@gmail.com>
2025-11-19 09:34:38 +0000

    Try fixing this error: `Console Error: ReactDOM.useFormState has been re

--- src/components/contact-form.tsx
+++ src/components/contact-form.tsx
@@ -1,6 +1,7 @@
 'use client';
 
-import { useFormState, useFormStatus } from 'react-dom';
+import { useActionState } from 'react';
+import { useFormStatus } from 'react-dom';
 import { useEffect } from 'react';
 import { ArrowRight } from 'lucide-react';
 
@@ -29,7 +30,7 @@ function SubmitButton() {
 }
 
 export function ContactForm() {
-  const [state, formAction] = useFormState(submitContactForm, initialState);
+  const [state, formAction] = useActionState(submitContactForm, initialState);
   const { toast } = useToast();
 
   useEffect(() => {
